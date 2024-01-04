7ea279f6f91fa2d5414fcae12340b82784e199d4
Brandom <brandon.rsantillan@gmail.com>
2024-01-03 22:17:11 -0500

    Add index.html, styles.css, and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import dotenv from 'dotenv';
-import dbConnection from './database/config';
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import { Server } from 'socket.io';
+import './sockets/socket';
 
 dotenv.config();
+const port = process.env.PORT;
+const publicPath = path.join(path.dirname(__dirname), 'public');
 
-dbConnection();
+const app = express();
+app.use(express.static(publicPath));
+
+const server = http.createServer(app);
+export const io = new Server(server);
+
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
