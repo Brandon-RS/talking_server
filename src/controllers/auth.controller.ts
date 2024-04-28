42311b828d002d68b5ed54e1d1584b5971b50633
Brandom <brandon.rsantillan@gmail.com>
2024-04-28 00:16:44 -0500

    Add centralized response handler for `auth.controller.ts`

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,10 @@ import { Request, Response } from 'express';
 
 import { generateJWT } from '../helpers/jwt';
 import logger from '../helpers/logger.helper';
+import {
+  getErrorResponse,
+  getSuccessResponse,
+} from '../helpers/response.helper';
 import usersSchema from '../models/users.schema';
 
 const User = usersSchema;
@@ -17,44 +21,34 @@ export const authUser = async (req: Request, res: Response) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({
-        success: false,
-        msg: 'Email or Password is not correct',
-      });
+      return res
+        .status(400)
+        .json(getErrorResponse('Email or Password is not correct'));
     }
 
     if (!user.verified) {
-      return res.status(400).json({
-        success: false,
-        msg: 'Email is not verified',
-      });
+      return res
+        .status(400)
+        .json(getErrorResponse('Please verify your account first'));
     }
 
     // Validate password
     const validPassword = bcrypt.compareSync(password, user.password);
 
     if (!validPassword) {
-      return res.status(400).json({
-        success: false,
-        msg: 'Email or Password is not correct',
-      });
+      return res
+        .status(400)
+        .json(getErrorResponse('Email or Password is not correct'));
     }
 
     // Generate JWT
     const token = await generateJWT(user.id);
 
-    res.json({
-      success: true,
-      user,
-      token,
-    });
+    return res.json(getSuccessResponse({ user, token }));
   } catch (error: any) {
     logger.error(`POST: api/auth - ${email} - ${error}`);
 
-    res.status(500).json({
-      success: false,
-      msg: 'Error authenticating user',
-    });
+    return res.status(500).json(getErrorResponse('Error authenticating user'));
   }
 };
 
@@ -65,10 +59,9 @@ export const renewToken = async (req: Request, res: Response) => {
 
   try {
     if (!uid) {
-      return res.status(400).json({
-        success: false,
-        msg: 'No user id',
-      });
+      return res
+        .status(400)
+        .json(getErrorResponse('Error renewing token, invalid user id'));
     }
 
     const token = await generateJWT(uid);
@@ -76,23 +69,17 @@ export const renewToken = async (req: Request, res: Response) => {
     const user = await User.findById(uid);
 
     if (!(user?.verified ?? false)) {
-      return res.status(400).json({
-        success: false,
-        msg: 'Email is not verified',
-      });
+      return res
+        .status(400)
+        .json(getErrorResponse('Please verify your account first'));
     }
 
-    res.json({
-      success: true,
-      user,
-      token,
-    });
+    return res.json(getSuccessResponse({ user, token }));
   } catch (error: any) {
     logger.error(`GET: api/renew-token - ${error}`);
 
-    res.status(500).json({
-      success: false,
-      msg: 'Error renewing token',
-    });
+    return res
+      .status(500)
+      .json(getErrorResponse('Error renewing token, please try again later'));
   }
 };
